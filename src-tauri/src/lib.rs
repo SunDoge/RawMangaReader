9714eb215456a9cb4697a8dc5378b342c7350383
SunDoge <triplez0@outlook.com>
2025-06-30 15:49:34 +0800

    update frontend

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,4 @@
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    // your code here
+}
--- src/pages/clipboard/index.tsx
+++ src/pages/clipboard/index.tsx
@@ -1,102 +1,114 @@
 import React, { useEffect, useState } from "react";
 import { listen } from "@tauri-apps/api/event";
 import { IMAGE_CHANGED, listenToClipboard } from "tauri-plugin-clipboard-api";
-import { invoke } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/core";
 import { Button, List, Switch } from "antd";
 import { Link } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import { appWindow } from "@tauri-apps/api/window"
+import { getCurrentWebviewWindow } from "@tauri-apps/api/webviewWindow";
+const appWindow = getCurrentWebviewWindow();
 
 type Result = {
-    id: string,
-    eventId: number,
-    imageBuffer: string // base64,
-    ocrText: string,
-}
+  id: string;
+  eventId: number;
+  imageBuffer: string; // base64,
+  ocrText: string;
+};
 
 type ClipboardImageEvent = {
-    id: number,
-    payload: {
-        value: string
-    }
-}
+  id: number;
+  payload: {
+    value: string;
+  };
+};
 
 export const ClipboardPage: React.FC<{}> = (_props) => {
+  const [unlisten, setUnlisten] = useState(() => () => {});
+  const [results, setResults] = useState<Result[]>([]);
 
+  const listenClipboard = async () => {
+    const u1 = await listen(IMAGE_CHANGED, (event: ClipboardImageEvent) => {
+      console.log(event);
+      const resultId = uuidv4();
+      const imageBuffer = event.payload.value;
+      const eventId = event.id;
+      setResults((results) => {
+        const result = {
+          id: resultId,
+          eventId: eventId,
+          imageBuffer: imageBuffer,
+          ocrText: "",
+        };
+        return [result, ...results];
+      });
 
-    const [unlisten, setUnlisten] = useState(() => () => { })
-    const [results, setResults] = useState<Result[]>([]);
+      invoke<string>("model_infer_base64", {
+        imageBuffer: imageBuffer,
+      }).then((text) => {
+        console.log(text);
+        setResults((results) =>
+          results.map((result) => {
+            if (result.id === resultId) {
+              result.ocrText = text;
+            }
+            return result;
+          }),
+        );
+      });
+    });
+    const u2 = await listenToClipboard();
+    console.log("listen to clipboard");
 
-    const listenClipboard = async () => {
-        const u1 = await listen(IMAGE_CHANGED, (event: ClipboardImageEvent) => {
-            console.log(event);
-            const resultId = uuidv4();
-            const imageBuffer = event.payload.value;
-            const eventId = event.id;
-            setResults((results) => {
-                const result = {
-                    id: resultId,
-                    eventId: eventId,
-                    imageBuffer: imageBuffer,
-                    ocrText: "",
-                };
-                return [result, ...results];
-            })
+    return () => {
+      u1();
+      u2();
+      console.log("unlisten to clipboard");
+    };
+  };
 
-            invoke<string>("model_infer_base64", {
-                imageBuffer: imageBuffer,
-            }).then((text) => {
-                console.log(text);
-                setResults((results) => results.map((result) => {
-                    if (result.id === resultId) {
-                        result.ocrText = text;
-                    }
-                    return result;
-                }))
-            })
-        });
-        const u2 = await listenToClipboard();
-        console.log('listen to clipboard');
+  // 注册一个unlisten上去
+  useEffect(() => unlisten, []);
 
-        return () => {
-            u1(); u2();
-            console.log('unlisten to clipboard')
-        }
+  const handleListen = (checked: boolean) => {
+    if (checked) {
+      listenClipboard().then((u) => {
+        setUnlisten(() => u);
+      });
+    } else {
+      unlisten();
     }
+  };
 
-    // 注册一个unlisten上去
-    useEffect(() => unlisten, [])
-
-    const handleListen = (checked: boolean) => {
-        if (checked) {
-            listenClipboard().then((u) => {
-                setUnlisten(() => u);
-            })
-        } else {
-            unlisten();
-        }
-    }
-
-    return (
-        <div>
-            <Button><Link to={"/home"}>Back</Link></Button>
-            <Switch checkedChildren="监听剪贴板" unCheckedChildren="开启监听剪贴板" onChange={handleListen}></Switch>
-            <Switch unCheckedChildren="always on top"
-                onChange={(checked) => appWindow.setAlwaysOnTop(checked)}
+  return (
+    <div>
+      <Button>
+        <Link to={"/home"}>Back</Link>
+      </Button>
+      <Switch
+        checkedChildren="监听剪贴板"
+        unCheckedChildren="开启监听剪贴板"
+        onChange={handleListen}
+      ></Switch>
+      <Switch
+        unCheckedChildren="always on top"
+        onChange={(checked) => appWindow.setAlwaysOnTop(checked)}
+      />
+      <List
+        itemLayout="horizontal"
+        dataSource={results}
+        renderItem={(item) => (
+          <List.Item>
+            <img
+              style={{
+                height: "200px",
+              }}
+              src={`data:image/png;base64,${item.imageBuffer}`}
+              alt=""
             />
-            <List
-                itemLayout="horizontal"
-                dataSource={results}
-                renderItem={(item) => (
-                    <List.Item>
-                        <img style={{
-                            height: "200px",
-                        }} src={`data:image/png;base64,${item.imageBuffer}`} alt="" />
-                        <p>OCR: {item.ocrText}</p>
-                    </List.Item>
-                )}
-            />
-        </div>
-    )
-}
-
+            <p>OCR: {item.ocrText}</p>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+};
