0e8da6c38fac2893ca183b95d671d3060395a2f6
SunDoge <triplez0@outlook.com>
2025-07-03 23:23:16 +0800

    prepare for image transform utils

--- src-tauri/crates/imgproc/src/lib.rs
+++ src-tauri/crates/imgproc/src/lib.rs
@@ -1,3 +1,25 @@
+use image::RgbImage;
+
+
+
+
+pub struct Bbox {
+    cx: f32,
+    cy: f32,
+    w: f32,
+    h: f32,
+}
+
+pub fn crop_and_resize(img: &RgbImage, bbox: &Bbox)  {
+    let x = (bbox.cx - bbox.w / 2.0) as u32;
+    let y = (bbox.cy - bbox.h / 2.0) as u32;
+    let w = bbox.w as u32;
+    let h = bbox.h as u32;
+
+}
+
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
